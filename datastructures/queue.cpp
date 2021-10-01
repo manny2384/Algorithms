3bc5c8d6b9c8075418790cca1368addbce3b41ec
manny2384 <gonzalezeh2015@gmail.com>
2021-10-01 12:18:45 -0700

    medical regression gif

---
+++ datastructures/queue.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+
+struct Node{
+    // value of node
+    int val;
+    // pointer to next node
+    Node * next = NULL;
+};
+
+int main(){
+    int n;
+    std::vector<int> A;
+    std::cin >> n;
+
+    srand(time(NULL));
+    for(int i=0; i<n; i++){
+        
+    }
+
+    std::cout << "The starting array is the following : \n";
+
+
+
+    return 0;
+}
